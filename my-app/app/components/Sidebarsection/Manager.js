f58d9bca9883aaaffc9049f2c8aa5820a926dc51
beku <146428492+Bekupollll@users.noreply.github.com>
2025-05-27 13:13:01 +0800

    papaa

--- my-app/app/components/Sidebarsection/Manager.js
+++ my-app/app/components/Sidebarsection/Manager.js
@@ -2,12 +2,6 @@ export default function Manager() {
 
   return (
     <div className="h-screen">
-      <iframe
-        src="/pdf/teacher.pdf"
-        width="100%"
-        height="100%"
-        style={{ border: 'none' }}
-      />
     </div>
   );
 }
